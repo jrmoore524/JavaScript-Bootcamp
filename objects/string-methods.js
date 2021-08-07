3b4f183f91d145c932ec58ce43c2d5bbfade910e
jrmoore524 <jrmoore524@gmail.com>
2021-08-07 09:36:57 -0500

    Create string-methods.js

---
+++ objects/string-methods.js
@@ -0,0 +1,32 @@
+//Print name length (length method)
+let name = ' Jesse Moore '
+console.log(name.length)
+
+//Convert to uppercase and print (upperCase method)
+name.toUpperCase();
+console.log(name.toUpperCase())
+
+//Convert to lowercase and print (lowerCase method)
+console.log(name.toLowerCase())
+
+//Check if the password includes 'password' (include method)
+let password = 'abc123asdf098'
+console.log(password.includes('password'))
+
+//Trim whitespace from name (trim method)
+console.log(name.trim())
+
+
+
+//Challenge (isValidPassword)
+//length is more than 8 - does not contain the word password
+
+//Create isValidPassword function and return boolean for valid password
+let isValidPassword = function (password) {
+   return password.length > 8 && !password.includes('password')
+}
+
+//Print if entered passwords are valid
+console.log(isValidPassword('asdfp'))
+console.log(isValidPassword('abc123!@#$%^&'))
+console.log(isValidPassword('asdfpasdfpoijpassword'))

6dab8ea78056e650527ad5f7270f6bf6d98ec637
jrmoore524 <jrmoore524@gmail.com>
2021-08-10 11:37:08 -0500

    notes update

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -1,43 +1,45 @@
+//Create account object
 const account = {
     name:'Jesse Moore',
     expenses: [],
     income: [],
+    //addExpense function
     addExpense: function (description, amount) {
         this.expenses.push({
             description: description,
             amount: amount
         })
     },
+    //addIncome function
     addIncome: function (description, amount) {
         this.income.push({
             description: description,
             amount: amount
         })
     }, 
+    //getAccountSummary
     getAccountSummary: function () {
         let totalExpenses = 0
         let totalIncome = 0
         let accountBalance = 0
-
+        
+        //total the expenses
         this.expenses.forEach(function (expense) {
             totalExpenses += expense.amount
         })
 
+        //total the income
         this.income.forEach(function (income) {
             totalIncome += income.amount
         })
 
+        //calculate the account balance
         accountBalance = totalIncome - totalExpenses
-       
+
+       //output the account summary
         return `${this.name} has a balance of $${accountBalance}. $${totalIncome} in income. $${totalExpenses} in expenses.`
     }
 }
-
-//Add income array to account
-//Add income method -> description, amount
-//Tweak get account summary
-
-//Jesse Moore has a balance of $10. $100 in  income. $90 in expenses.
 
 account.addExpense('Rent', 950)
 account.addExpense('Coffee', 2)

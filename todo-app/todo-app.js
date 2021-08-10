7214cfc04f202bd7234f4b08900dcf61936f8b90
jrmoore524 <jrmoore524@gmail.com>
2021-08-10 13:39:04 -0500

    Update todo-app.js

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,17 +15,24 @@ const todos = [{
     text: 'Exercise',
     completed: true
 }]
-
-//You have 2 todos left (p element)
-//Add a p for each todo above (use text value)
-
+//Get the incompleted todos
 const incompleteTodos = todos.filter(function (todo) {
     return !todo.completed
 })
 
+//Show a summary of incomplete todos on the page
 const summary = document.createElement('h2') 
 summary.textContent = `You have ${incompleteTodos.length} todos left`
 document.querySelector('body').appendChild(summary)
+
+//Show all of the todos on the page
+todos.forEach(function (todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
+})
+
+
 
 
 

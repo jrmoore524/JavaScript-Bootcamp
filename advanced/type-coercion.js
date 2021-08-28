3874d075f7b48d8187f96e803714f714c9ee96d2
jrmoore524 <jrmoore524@gmail.com>
2021-08-28 10:58:40 -0500

    try-catch update

---
+++ advanced/type-coercion.js
@@ -0,0 +1,7 @@
+//Type Coercion - a string, a number, a boolean
+
+const value = true + 12
+
+const type = typeof value
+console.log(type)
+console.log(value)
--- functions/grade-calc.js
+++ functions/grade-calc.js
@@ -3,33 +3,35 @@
 //15/20 -> you got a C (75%)!
 //A 90-100, B 80-89, C 70-79, D 60-69, F 0-59
 
-//Set variables for score and total score
-let score = 15
-let totalScore = 20
-
 //gradeCalc function
 const gradeCalc = function (score, totalScore) {
+    if (typeof score !== 'number' || typeof totalScore !== 'number') {
+        throw Error('Please provide numbers only')
+    }
+
     const percent = (score / totalScore) * 100
     let letterGrade = ''
 
-    //Set percentage to a letterGrade
     if (percent >= 90) {
-       letterGrade = 'A'
-    }else if (percent >= 80) {
+        letterGrade = 'A'
+    } else if (percent >= 80) {
         letterGrade = 'B'
-    }else if (percent >= 70) {
+    } else if (percent >= 70) {
         letterGrade = 'C'
-    }else if (percent >= 60) {
+    } else if (percent >= 60) {
         letterGrade = 'D'
-    }else {
+    } else {
         letterGrade = 'F'
     }
-    //Return the template string with letter grade and percent
+         
     return `You got a ${letterGrade} (${percent}%)!`
 }
 
-//Set result to functions returned value
-const result = gradeCalc(score, totalScore)
+try {
+    const result = gradeCalc(9,20)
+    console.log(result)
+} catch (error) {
+    console.log(error.message)
+}
 
-//Output the result
-console.log(result)
+    

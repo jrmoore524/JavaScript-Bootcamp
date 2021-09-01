c83e3da686e0484d0d3c024260df9a68a3679ee2
jrmoore524 <jrmoore524@gmail.com>
2021-09-01 17:44:14 -0500

    hangman update

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -6,15 +6,7 @@ const Hangman = function (word, remainingGuesses) {
 }
 
 Hangman.prototype.calculateStatus = function () {
-    let finished = true
-
-    this.word.forEach((letter) => {
-        if (this.guessedLetters.includes(letter)) {
-
-        } else {
-            finished = false
-        }
-    })
+    const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
 
     if (this.remainingGuesses === 0) {
         this.status = 'failed'
@@ -22,6 +14,16 @@ Hangman.prototype.calculateStatus = function () {
         this.status = 'finished'
     } else {
         this.status = 'playing'
+    }
+}
+
+Hangman.prototype.getStatusMessage = function () {
+    if (this.status === 'playing') {
+        return `Guesses left: ${this.remainingGuesses}`
+    } else if (this.status === 'failed') {
+        return `Nice try! The word was "${this.word.join('')}".`
+    } else {
+        return 'Great work! You guessed the word.'
     }
 }
 
@@ -43,6 +45,10 @@ Hangman.prototype.makeGuess = function (guess) {
     guess = guess.toLowerCase()
     const isUnique = !this.guessedLetters.includes(guess)
     const isBadGuess = !this.word.includes(guess)
+
+    if (this.status !== 'playing') {
+        return
+    }
 
     if (isUnique) {
         this.guessedLetters.push(guess)

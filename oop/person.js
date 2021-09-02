ea0252f902bb322aee6d5c46c4aab6134bab779a
jrmoore524 <jrmoore524@gmail.com>
2021-09-01 19:40:00 -0500

    hangman new class update

--- oop/person.js
+++ oop/person.js
@@ -1,24 +1,24 @@
-const Person = function (firstName, lastName, age, likes = []) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
-}
-
-Person.prototype.getBio = function () {
-    let bio = `${this.firstName} is ${this.age}.`
+class Person {
+    constructor (firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
 
     this.likes.forEach((like) => {
         bio += ` ${this.firstName} likes ${like}.`
     })
 
     return bio
-}
-
-Person.prototype.setName = function (fullName) {
-    const names = fullName.split(' ')
-    this.firstName = names[0]
-    this.lastName = names[1]
+    }
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
 }
 
 const me = new Person('Jesse', 'Moore', 35, ['Coding', 'Fishing'])

2a3519cf66eec2f3a320a1408b23d458131dd7f1
jrmoore524 <jrmoore524@gmail.com>
2021-09-03 12:31:02 -0500

    hangman and person update

--- oop/person.js
+++ oop/person.js
@@ -8,8 +8,8 @@ class Person {
     getBio() {
         let bio = `${this.firstName} is ${this.age}.`
 
-    this.likes.forEach((like) => {
-        bio += ` ${this.firstName} likes ${like}.`
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
     })
 
     return bio
@@ -21,9 +21,33 @@ class Person {
     }
 }
 
-const me = new Person('Jesse', 'Moore', 35, ['Coding', 'Fishing'])
-me.setName('Madeleine Moore')
-console.log(me.getBio())
+class Employee extends Person {
+    constructor (firstName, lastName, age, position, likes) {
+        super(firstName, lastName, age, likes)
+        this.position = position
+    }
+    getBio () {
+        return `${this.firstName} ${this.lastName} is a ${this.position}.`
+    }
+    getYearsLeft () {
+        return 65 - this.age
+    }
+}
 
-const person2 = new Person('Tara', 'Moore', 35)
-console.log(person2.getBio())
+class Student extends Person {
+    constructor (firstName, lastName, age, grade, likes) {
+        super(firstName, lastName, age, likes)
+        this.grade = grade
+    }
+    updateGrade(change) {
+        this.grade += change
+    }
+    getBio () {
+        const status = this.grade >= 70 ? 'passing' : 'failing'
+        return `${this.firstName} is ${status} the class.`
+    }
+}
+const student1 = new Student ('Barry', 'Boyd', 65, 75, [])
+console.log(student1.getBio())
+student1.updateGrade(-25)
+console.log(student1.getBio())
